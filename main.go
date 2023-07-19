4358ef63d203d017674ed7fa5a5767c3b9156baf
Matheus Silva <mathsant_99@hotmail.com>
2023-07-19 15:26:55 -0300

    using select to get message

--- main.go
+++ main.go
@@ -1,52 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
-func makeRequest(url string, result chan<- *http.Response) {
-	client := http.Client{
-		Timeout: 1 * time.Second,
-	}
+// FAVOR LER ESSA MENSAGEM:
 
-	response, err := client.Get(url)
+// A api para pegar o CEP do CDN informado no desafio não está respondendo.
+
+type CepResponse struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+func makeRequest(url string, result chan<- *http.Response) {
+	response, err := http.Get(url)
 	if err != nil {
 		result <- nil
 		return
 	}
-
 	result <- response
 }
 
-func fetchData(cep string) *http.Response {
-	url1 := fmt.Sprintf("https://cdn.apicep.com/file/apicep/%s.json", cep)
-	url2 := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", cep)
-
-	result := make(chan *http.Response, 2)
-
-	go makeRequest(url1, result)
-	go makeRequest(url2, result)
-
-	for i := 0; i < 2; i++ {
-		response := <-result
-		if response != nil {
-			return response
-		}
-	}
-
-	return nil
-}
-
 func main() {
-	cep := "36904-278"
+	channel1 := make(chan *http.Response)
+	channel2 := make(chan *http.Response)
 
-	response := fetchData(cep)
+	url1 := fmt.Sprintf("https://cdn.apicep.com/file/apicep/%s.json", "36904278")
+	url2 := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", "36904278")
 
-	if response != nil {
-		fmt.Printf("API: %s\n", response.Request.URL)
-	} else {
-		fmt.Println("Nenhuma resposta recebida dentro do tempo limite.")
+	go makeRequest(url1, channel1) // CDN
+	go makeRequest(url2, channel2) // Via Cep
+
+	select {
+	case msg := <-channel1: // cdn
+		var data CepResponse
+		json.NewDecoder(msg.Body).Decode(&data)
+		fmt.Printf("Received from CDN: ID: %s | CEP Resgatado: %s\n", msg.Request.URL, data.Cep)
+	case msg := <-channel2: // via cep
+		var data CepResponse
+		json.NewDecoder(msg.Body).Decode(&data)
+		fmt.Printf("Received from Via CEP: URL: %s | CEP Resgatado: %s\n", msg.Request.URL, data.Cep)
+	case <-time.After(time.Second * 1):
+		println("Timeout!")
 	}
 }
