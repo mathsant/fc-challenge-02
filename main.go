6dc411d3e06a88dba0ab9a3082f92e34213e6f91
Matheus Silva <mathsant_99@hotmail.com>
2023-07-20 10:10:09 -0300

    fix: cdn via cep url

--- main.go
+++ main.go
@@ -7,11 +7,7 @@ import (
 	"time"
 )
 
-// FAVOR LER ESSA MENSAGEM:
-
-// A api para pegar o CEP do CDN informado no desafio não está respondendo.
-
-type CepResponse struct {
+type ViaCepResponse struct {
 	Cep         string `json:"cep"`
 	Logradouro  string `json:"logradouro"`
 	Complemento string `json:"complemento"`
@@ -22,6 +18,17 @@ type CepResponse struct {
 	Gia         string `json:"gia"`
 	Ddd         string `json:"ddd"`
 	Siafi       string `json:"siafi"`
+}
+
+type CDNViaCepResponse struct {
+	Code       string `json:"code"`
+	State      string `json:"state"`
+	City       string `json:"city"`
+	District   string `json:"district"`
+	Address    string `json:"address"`
+	Status     int    `json:"status"`
+	Ok         bool   `json:"ok"`
+	StatusText string `json:"statusText"`
 }
 
 func makeRequest(url string, result chan<- *http.Response) {
@@ -37,7 +44,7 @@ func main() {
 	channel1 := make(chan *http.Response)
 	channel2 := make(chan *http.Response)
 
-	url1 := fmt.Sprintf("https://cdn.apicep.com/file/apicep/%s.json", "36904278")
+	url1 := fmt.Sprintf("https://cdn.apicep.com/file/apicep/%s.json", "36904-278")
 	url2 := fmt.Sprintf("http://viacep.com.br/ws/%s/json/", "36904278")
 
 	go makeRequest(url1, channel1) // CDN
@@ -45,11 +52,11 @@ func main() {
 
 	select {
 	case msg := <-channel1: // cdn
-		var data CepResponse
+		var data CDNViaCepResponse
 		json.NewDecoder(msg.Body).Decode(&data)
 		fmt.Printf("Received from CDN: ID: %s | Dados Resgatados: %v\n", msg.Request.URL, data)
 	case msg := <-channel2: // via cep
-		var data CepResponse
+		var data ViaCepResponse
 		json.NewDecoder(msg.Body).Decode(&data)
 		fmt.Printf("Received from Via CEP: URL: %s | Dados Resgatados: %v\n", msg.Request.URL, data)
 	case <-time.After(time.Second * 1):
